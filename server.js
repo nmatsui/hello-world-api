655c9f74f4a33ddb5e4ba6b3e50292831d8634d9
Nobuyuki Matsui <nobuyuki.matsui@gmail.com>
2018-04-20 18:22:59 +0900

    add tests

--- server.js
+++ server.js
@@ -7,13 +7,26 @@
  * SPDX-License-Identifier: MIT
  */
 
-const MESSAGE = process.env.MESSAGE || "hello world!";
-const PORT = process.env.PORT || "3000"
 const http = require('http');
+const logger = require('log4js').getLogger();
+logger.level = 'debug';
 
-http.createServer((request, response) => {
-  console.log(`Received request for URL: ${request.url}`);
-  response.writeHead(200, {'Content-Type': 'application/json'});
-  response.end(JSON.stringify({'message': MESSAGE}));
-}).listen(PORT);
-console.log(`Start server at port ${PORT}`);
+const DEFAULT_MESSAGE = 'hello world!';
+const DEFAULT_PORT = '3000';
+const MESSAGE = process.env.MESSAGE || DEFAULT_MESSAGE;
+var PORT = process.env.PORT || DEFAULT_PORT;
+
+let iPort = Number.parseInt(PORT);
+if (!Number.isInteger(iPort) || iPort < 0 || iPort > 65535) {
+    PORT = DEFAULT_PORT;
+}
+
+const server = http.createServer((request, response) => {
+    logger.info(`Received request for URL: ${request.url}`);
+    response.writeHead(200, {'Content-Type': 'application/json'});
+    response.end(JSON.stringify({'message': MESSAGE}));
+});
+server.listen(PORT);
+exports.started = server;
+exports.listenPort = PORT;
+logger.info(`Start server at port ${PORT}`);
