af5cce0da9f6cda7a00641ba8ff3d396d3baa683
Nobuyuki Matsui <nobuyuki.matsui@gmail.com>
2018-05-31 11:41:35 +0900

    log request header and body

--- src/server.js
+++ src/server.js
@@ -16,6 +16,13 @@ logger.level = config.logLevel;
 
 module.exports = http.createServer((request, response) => {
     logger.info(`Received ${request.method} request to: ${request.url}`);
+    logger.info(`Received header: ${JSON.stringify(request.headers)}`);
+    let body = [];
+    request.on('data', (chunk) => {
+        body.push(chunk);
+    }).on('end', () => {
+        logger.info(`Received body: ${Buffer.concat(body).toString()}`);
+    });
     let result = JSON.stringify({'message': config.message});
     response.writeHead(200, {'Content-Type': 'application/json'});
     logger.info(`Respond JSON: ${result}`);
